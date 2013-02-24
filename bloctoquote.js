113cb1b75362be45c1f1df090eee38bd7d7a94ed
Angelo Stavrow <contact@angelostavrow.com>
2013-02-24 17:21:34 -0500

    Initial release.

---
+++ bloctoquote.js
@@ -0,0 +1,26 @@
+// Bloctoquote Chrome Extension
+
+// Set up the context menu item upon installation.
+chrome.contextMenus.create({
+	"id": "bq",
+	"title": "Bloctoquote",
+	"contexts": ["page", "selection"]
+});
+
+chrome.contextMenus.onClicked.addListener(clickHandler);
+
+function clickHandler(info, tab)  {
+	var pageLink = info.pageUrl;
+	var pageTitle = tab.title;
+	var outputText = unescape('{% blockquote Source ' + pageLink + ' ' + pageTitle + ' %}\r\n' + escape(info.selectionText) + '\r\n' + '{% endblockquote %}');
+	console.log(outputText);
+	
+	bg = chrome.extension.getBackgroundPage();
+	clipboardholder= bg.document.getElementById("clipboardholder");
+	clipboardholder.style.display = "block";
+	clipboardholder.value = outputText;
+	clipboardholder.select();
+	bg.document.execCommand("Copy");
+	clipboardholder.style.display = "none";
+	alert("Blockquote created and copied to clipboard.");
+}
